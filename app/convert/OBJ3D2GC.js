db1111324c7826eff309715c754015dcd28314d3
Dima <dmitryl0626@gmail.com>
2020-08-25 18:31:18 +0200

    update slicing for gcode

--- app/convert/OBJ3D2GC.js
+++ app/convert/OBJ3D2GC.js
@@ -3,6 +3,10 @@ const config = require("../singleton/config");
 function getMeshGeometry(file){
   let geometry = file.data.geometry.clone();
   geometry.center();
+  geometry.computeBoundingBox();
+
+  let box = geometry.boundingBox;
+  let zOffset = Math.abs(box.min.z);
 
   let toRad = Math.PI / 180;
 
@@ -15,41 +19,52 @@ function getMeshGeometry(file){
   geometry.rotateY(angleZ);
   geometry.rotateZ(angleY);
 
+  geometry.translate(0, zOffset, 0);
+
   geometry.scale(scale, scale, scale);
 
   return geometry;
 }
 
-function getSizeOfGeometry(geometry){
+function getSize(file){
 
-  geometry.computeBoundingBox();
+  let geometry = getMeshGeometry(file);
 
   let box = geometry.boundingBox;
   console.log(box.min, box.max);
 
-  return box;
+  return {
+    width: Math.abs(box.min.x) * 2,
+    height: Math.abs(box.min.z) * 2,
+    depth: file.material.depth
+  };
+}
+
+function compareLayer(l1, l2){
+  console.log("compare");
+
+  for(let i = 0; i < l1.imageData.data.length; i++)
+    if(l1.imageData.data[i] != l2.imageData.data[i])
+      return false;
+
+  return true;
 }
 
 async function getLayers(param){
-  //box bounder
-  let box = param.sizeOfMesh;
-
-  //sizes count
-  let deviceZ = config.getDevZ();
-  let width = Math.abs(box.min.x) * 2;
-  let height = Math.abs(box.min.z) * 2;
-  let depth = Math.abs(box.min.y) * 2;
+  let width = param.size.width;
+  let height = param.size.height;
+  let depth = param.size.depth;
 
   //camera
-  camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 0, deviceZ);
-  camera.position.set(0, deviceZ, 0);
+  camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 0, depth + 1);
+  camera.position.set(0, depth + 1, 0);
   camera.lookAt(0, 0, 0);
 
   //renderer
   let renderer = param.renderer;
   renderer.setSize(width, height);
 
-  let images = [];
+  let layers = [];
   let step = 0.1;
 
   for(let i = depth; i >= 0; i -= step)
@@ -62,13 +77,13 @@ async function getLayers(param){
     let planeGeometry = new THREE.BoxGeometry(width, step, height);
     planeGeometry.center();
     planeGeometry.translate(0, i, 0);
-    planeGeometry.translate(0, -depth / 2, 0);
 
     let planeBSP = new ThreeBSP(planeGeometry);
     planeBSP = planeBSP.subtract(param.geometryBSP);
 
     let mesh = planeBSP.toMesh();
     if(mesh.geometry.vertices.length == 0) break; //exit if result mesh is empty
+
     scene.add(mesh);
 
     //render
@@ -77,14 +92,21 @@ async function getLayers(param){
     img.width = width;
     img.height = height;
 
-    images.push({
+    let newLayer = {
       img: img,
       imageData: getImageData(img),
       layer: i
-    });
+    };
+
+    if(layers.length > 0 && compareLayer(layers[layers.length - 1], newLayer)){ //check if previous layer has same geometry
+      layers[layers.length - 1].layer = i;
+    }
+    else{
+      layers.push(newLayer);
+    }
   }
 
-  return images;
+  return layers;
 }
 
 function loadImage(src){
@@ -111,7 +133,7 @@ function getImageData(img){
 exports.getGcode = async function(file){
 
   let geometry = getMeshGeometry(file);
-  let size = getSizeOfGeometry(geometry); // return Three.Box3
+  let size = getSize(file);
   let renderer = new THREE.WebGLRenderer({
     logarithmicDepthBuffer: true
   });
@@ -120,7 +142,7 @@ exports.getGcode = async function(file){
   let layers = await getLayers(
   {
     geometryBSP: new ThreeBSP(geometry),
-    sizeOfMesh: size,
+    size: size,
     renderer: renderer
   });
 
@@ -129,6 +151,7 @@ exports.getGcode = async function(file){
   for(let i = 0; i < layers.length; i++)
     console.image(layers[i].img.src);
 
+  gcode = [];
 
-  return null;
+  return gcode;
 }
